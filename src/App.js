5544970163d2d3b561aea701113dfed1c3561ffc
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-10-05 17:45:00 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+// import './App.css';
 
 function App() {
+
+  const [count,setCount] = useState(0);
+  const [value,setValue] = useState('AAA');
+
+  function increment() {
+   
+    setCount(count+1);
+  };
+  function decrement() {
+    setCount(count-1);
+   
+  };
+  function changeHandler(e){
+
+    setValue(e.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Cont:{count}
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <br/>
+      InpitValue: {value}
+      <br/>
+      <input type="text" value={value} onChange={(e)=>{changeHandler(e)}}></input>
+      
     </div>
   );
 }
