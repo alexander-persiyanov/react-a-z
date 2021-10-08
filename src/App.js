29986a99e26834e7dc7de9d47496387873941bbe
Alexander Persiyanov <persiyanov1990@hotmail.com>
2021-10-09 00:40:40 +0200

    add sort component

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import PostForm from './components/PostForm';
 
 // import './App.css';
 import  './styles/App.css'
+import MySelect from './components/UI/mySelect/mySelect';
 function App() {
 
   const [posts,setPosts] = useState([
-    {id:1,title:"Title text",body:"body text"},
-    {id:2,title:"Title text",body:"body text"},
-    {id:3,title:"Title text",body:"body text"},
-    {id:4,title:"Title text",body:"body text"},
+    {id:1,title:"Title 3",body:"D"},
+    {id:2,title:"Title 2",body:"A"},
+    {id:3,title:"Title 1",body:"C"},
+    {id:4,title:"Title 4",body:"B"},
   ]);
+
+  const [selectedSort,setSelectedSort] = useState("");
  
   
 
@@ -32,6 +35,14 @@ function App() {
   setPosts(posts.filter(p=>p.id !== post.id));
  }
 
+ const sortPosts = (sort)=>{
+    setSelectedSort(sort);
+   
+    //sort doesnt return new array but return same sorted array
+    setPosts( [...posts].sort((a,b)=>{ return a[sort].localeCompare(b[sort]); }) );
+
+ }
+
  
   
   
@@ -40,6 +51,7 @@ function App() {
     <Counter/>
     <ClassCounter/>
     <br/>
+    
     <form>
       <div>
         <p><strong> Value From Input with useRef:</strong> {valueInputRef}</p>  
@@ -48,14 +60,33 @@ function App() {
           ref={bodyInputRef} 
           type="text" 
           placeholder="insert value" 
-          onChange={(e)=>{ setValueInputRef(bodyInputRef.current.value);}}
+          onChange={(e)=>{ sortPosts() }}
         />
       </div>
       <br/>
 
     </form>
+
+    
        
     <PostForm create={createPost} ></PostForm>
+    <br/>
+    <hr/>
+    <br/>
+    <MySelect 
+      value={selectedSort}
+      defaultValue="Sort to" 
+      onChange={sortPosts}
+      options={[
+        {name:"title",value:"title"},
+        {name:"body",value:"body"}
+      ]}
+    />
+      
+      
+    <br/>
+    <hr/>
+    <br/>
     {posts.length !== 0 ? ( <PostList posts={posts} remove={removePost}></PostList>) : (<p style={{textAlign:'center', color:'red'}}><big> Posts not founds</big> </p>)  }
    
 
